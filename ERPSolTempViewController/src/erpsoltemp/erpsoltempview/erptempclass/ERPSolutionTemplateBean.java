5f5ae7af20a10fb6c104b66e0e98cbf9da67b72e
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-12 22:58:21 +0500

    INITIAL COMMIT

---
+++ ERPSolTempViewController/src/erpsoltemp/erpsoltempview/erptempclass/ERPSolutionTemplateBean.java
@@ -0,0 +1,21 @@
+package erpsoltemp.erpsoltempview.erptempclass;
+
+public class ERPSolutionTemplateBean {
+    private String lERPActivityName;
+    private String lERPActionRuntime;//runtime activite name, this will be opened
+    private String lERPAllowEdit;
+    private String lERPAllowAdd;
+    private String lERPAllowDelete;
+    private String lERPAllowCancel;
+    private String lERPAllowSupervise;
+    private String lERPAllowUnsupervise;
+    private String lERPIsAllow;
+    private String lERPAllowEditOther;
+    private String lERPAllowPrint;
+    private String lERPAllowScan;
+    private String lERPModuleAction;//MODULE_ACTION id from SYS_MODULE_DETAIL
+  
+    public ERPSolutionTemplateBean() {
+        super();
+    }
+}
