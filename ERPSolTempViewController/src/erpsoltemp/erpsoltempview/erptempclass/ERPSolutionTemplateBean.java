9fc58f4872f42417b63a5af97db03a446a41ed3a
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-12 23:07:38 +0500

    commit

--- ERPSolTempViewController/src/erpsoltemp/erpsoltempview/erptempclass/ERPSolutionTemplateBean.java
+++ ERPSolTempViewController/src/erpsoltemp/erpsoltempview/erptempclass/ERPSolutionTemplateBean.java
@@ -1,21 +1,109 @@
 package erpsoltemp.erpsoltempview.erptempclass;
 
 public class ERPSolutionTemplateBean {
-    private String lERPActivityName;
-    private String lERPActionRuntime;//runtime activite name, this will be opened
-    private String lERPAllowEdit;
-    private String lERPAllowAdd;
-    private String lERPAllowDelete;
-    private String lERPAllowCancel;
-    private String lERPAllowSupervise;
-    private String lERPAllowUnsupervise;
-    private String lERPIsAllow;
-    private String lERPAllowEditOther;
-    private String lERPAllowPrint;
-    private String lERPAllowScan;
-    private String lERPModuleAction;//MODULE_ACTION id from SYS_MODULE_DETAIL
+    private String lERPSolActivityName;
+    private String lERPSolActionRuntime;//runtime activite name, this will be opened
+    private String lERPSolAllowEdit;
+    private String lERPSolAllowAdd;
+    private String lERPSolAllowDelete;
+    private String lERPSolAllowCancel;
+    private String lERPSolAllowSubmit;
+    private String lERPSolAllowUnSubmit;
+    private String lERPSolIsAllow;
+    private String lERPSolAllowPrint;
+    private String lERPModuleId;//MODULE_ACTION id from SYS_PROGRAM
   
     public ERPSolutionTemplateBean() {
         super();
     }
+
+
+    public void setLERPSolActivityName(String lERPSolActivityName) {
+        this.lERPSolActivityName = lERPSolActivityName;
+    }
+
+    public String getLERPSolActivityName() {
+        return lERPSolActivityName;
+    }
+
+    public void setLERPSolActionRuntime(String lERPSolActionRuntime) {
+        this.lERPSolActionRuntime = lERPSolActionRuntime;
+    }
+
+    public String getLERPSolActionRuntime() {
+        return lERPSolActionRuntime;
+    }
+
+    public void setLERPSolAllowEdit(String lERPSolAllowEdit) {
+        this.lERPSolAllowEdit = lERPSolAllowEdit;
+    }
+
+    public String getLERPSolAllowEdit() {
+        return lERPSolAllowEdit;
+    }
+
+    public void setLERPSolAllowAdd(String lERPSolAllowAdd) {
+        this.lERPSolAllowAdd = lERPSolAllowAdd;
+    }
+
+    public String getLERPSolAllowAdd() {
+        return lERPSolAllowAdd;
+    }
+
+    public void setLERPSolAllowDelete(String lERPSolAllowDelete) {
+        this.lERPSolAllowDelete = lERPSolAllowDelete;
+    }
+
+    public String getLERPSolAllowDelete() {
+        return lERPSolAllowDelete;
+    }
+
+    public void setLERPSolAllowCancel(String lERPSolAllowCancel) {
+        this.lERPSolAllowCancel = lERPSolAllowCancel;
+    }
+
+    public String getLERPSolAllowCancel() {
+        return lERPSolAllowCancel;
+    }
+
+    public void setLERPSolAllowSubmit(String lERPSolAllowSubmit) {
+        this.lERPSolAllowSubmit = lERPSolAllowSubmit;
+    }
+
+    public String getLERPSolAllowSubmit() {
+        return lERPSolAllowSubmit;
+    }
+
+    public void setLERPSolAllowUnSubmit(String lERPSolAllowUnSubmit) {
+        this.lERPSolAllowUnSubmit = lERPSolAllowUnSubmit;
+    }
+
+    public String getLERPSolAllowUnSubmit() {
+        return lERPSolAllowUnSubmit;
+    }
+
+    public void setLERPSolIsAllow(String lERPSolIsAllow) {
+        this.lERPSolIsAllow = lERPSolIsAllow;
+    }
+
+    public String getLERPSolIsAllow() {
+        return lERPSolIsAllow;
+    }
+
+    public void setLERPSolAllowPrint(String lERPSolAllowPrint) {
+        this.lERPSolAllowPrint = lERPSolAllowPrint;
+    }
+
+    public String getLERPSolAllowPrint() {
+        return lERPSolAllowPrint;
+    }
+
+    public void setLERPModuleId(String lERPModuleId) {
+        this.lERPModuleId = lERPModuleId;
+    }
+
+    public String getLERPModuleId() {
+        return lERPModuleId;
+    }
+
 }
